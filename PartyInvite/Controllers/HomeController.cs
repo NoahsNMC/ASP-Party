81aee2dbb50aaf2119b890eb434eb1a2b7e059b7
NoahFlowa <osterhoutn@gmail.com>
2019-01-24 13:49:29 -0500

    Initial Commit

---
+++ PartyInvite/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PartyInvite.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult RSVPForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult RSVPForm(string name, string email, string phone)
+        {
+            ViewBag.Message = "Thanks, " + name + " we got your message!";
+            return View("ThankYou");
+        }
+
+    }
+}
